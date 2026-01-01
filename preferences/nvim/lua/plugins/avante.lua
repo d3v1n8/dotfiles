37f3ccb14e8d4d919770bc2478bd5be0d8bb8c8d
d3v1n8 <d3v1n8@users.noreply.github.com>
2026-01-01 18:56:06 +0900

    Add avante.nvim with Claude support

---
+++ preferences/nvim/lua/plugins/avante.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false,
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = { file_types = { "markdown", "Avante" } },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    opts = {
+      provider = "claude",
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-sonnet-4-20250514",
+        max_tokens = 4096,
+      },
+    },
+  },
+}
