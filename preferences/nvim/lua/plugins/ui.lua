7aee0efd4609e250ee1e72e9cc875df3eb132bb1
d3v1n8 <d3v1n8@users.noreply.github.com>
2026-01-01 18:22:18 +0900

    Always show bufferline even with single buffer

---
+++ preferences/nvim/lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+return {
+  -- Bufferline: always show buffer tab
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
+  },
+}
