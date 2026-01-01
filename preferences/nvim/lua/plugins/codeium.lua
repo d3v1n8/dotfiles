6d2ca485ac5f67b1fa13cf38ea3a6263d1688279
d3v1n8 <d3v1n8@users.noreply.github.com>
2026-01-01 19:03:27 +0900

    Fix Codeium: use codeium.vim only (no nvim-cmp dependency)

--- preferences/nvim/lua/plugins/codeium.lua
+++ preferences/nvim/lua/plugins/codeium.lua
@@ -1,16 +1,5 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({})
-    end,
-  },
-
-  -- Virtual text for inline suggestions
+  -- Codeium: inline AI code suggestions (ghost text)
   {
     "Exafunction/codeium.vim",
     event = "BufEnter",
