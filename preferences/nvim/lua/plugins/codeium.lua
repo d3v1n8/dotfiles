84f99ef6bb98f79102081ada5815d44001c09b2a
d3v1n8 <d3v1n8@users.noreply.github.com>
2026-01-01 19:01:40 +0900

    Add Codeium for inline AI code suggestions

---
+++ preferences/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({})
+    end,
+  },
+
+  -- Virtual text for inline suggestions
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    config = function()
+      vim.g.codeium_disable_bindings = 1
+      -- Tab to accept suggestion
+      vim.keymap.set("i", "<Tab>", function()
+        if vim.fn["codeium#Accept"]() ~= "" then
+          return vim.fn["codeium#Accept"]()
+        else
+          return "<Tab>"
+        end
+      end, { expr = true, silent = true })
+      -- Ctrl+] to next suggestion
+      vim.keymap.set("i", "<C-]>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      -- Ctrl+[ to previous suggestion
+      vim.keymap.set("i", "<C-[>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      -- Ctrl+x to clear
+      vim.keymap.set("i", "<C-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
+    end,
+  },
+}
